dc48867ed776eb293051ea8b11d6dd5f261b6691
DClark <duncan.clark528@gmail.com>
2026-03-07 21:12:33 -0500

    Board scaffolding

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,56 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
 
-
+//row array
+function Row() {
   return (
-    <h1>SECRET PASSWORD</h1>
-  )
+    <>
+    <section className='row'>
+    <div className='square'>1</div>
+    <div className='square'>2</div>
+    <div className='square'>3</div>
+    <div className='square'>4</div>
+    <div className='square'>5</div>
+    </section>
+    </>
+  );
 }
 
-export default App
+
+//icebox: Interactive keyboard
+function Keyboard () {
+  return(
+    <button className='key'>This will be a keyboard eventually</button>
+  );
+};
+
+
+
+
+
+export default function Board() {
+
+  //test winning word
+  const winningWord = "Start";
+
+  return (
+<>
+<div className='title'>
+<h1>SECRET PASSWORD</h1>
+</div>
+<section className='board'>
+    <Row className='guess-1'/>
+    <Row className='guess-2'/>
+    <Row className='guess-3'/>
+    <Row className='guess-4'/>
+    <Row className='guess-5'/>
+    <Row className='guess-6'/>
+</section>
+<section>
+  <Keyboard />
+</section>
+</>
+  );
+};
