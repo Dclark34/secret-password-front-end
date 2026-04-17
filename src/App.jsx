be342b0b9bf3844dc2819e2ded0475ecef640eb0
DClark <duncan.clark528@gmail.com>
2026-04-17 18:27:04 -0400

    started handle submit function

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,71 @@ import { useState } from 'react'
 import './App.css'
 
 
+ function App() {
+  //test winning word
+  const winningWord = "psych";
+
+  //winner state
+const [isWinner, setIsWinner] = useState(false);
+
+//total guesses
+const [totalGuesses, setTotalGuesses] = useState([]);
+
+// state for user input guess
+  const [currentGuess, setCurrentGuess] = useState([]);
+
+
+  //event handler for user input
+const handleChange = (event) => {
+  let newLetter = event.target.value.toUpperCase().split(''); //splits input by space between letters.
+  const newGuessArray = newLetter; //pushes split letter into its own index in array.
+  console.log(newGuessArray);
+  setCurrentGuess(newGuessArray);//sets user input as an array. Will use this array to map to Board
+};
+
+
+//handle guess submit
+const handleSubmit = (event) => {
+  event.preventDefault() //must be added because in HTML submit navigates away from page. 
+  console.log(event); 
+};
+
+
+
+  return (
+<>
+<div className='title'>
+<h1>SECRET PASSWORD</h1>
+</div>
+<section className='board'>
+    <Board currentGuess={currentGuess}/>
+</section>
+<section className='user-input-container'>
+<form>
+  <label htmlFor="currentGuess">Your Guess: </label>
+  <input 
+  id="currentGuess" 
+  name="currentGuess" 
+  maxLength={5}
+  type="text"  
+  onChange={handleChange} 
+  onSubmit={handleSubmit}
+  />
+  <button type="submit">Submit</button>
+  </form>
+</section>
+</>
+  );
+};
+
+
+
+
 //board
-function Board({ currentGuess }) {
+function Board({ currentGuess, totalGuesses }) {
   return (
     <>
-    <Row className="guess-one" currentGuess={currentGuess}/>
+    <Row className="guess-one" currentGuess={currentGuess} totalGuesses={totalGuesses}/>
     </>
   )
 }
@@ -30,47 +90,4 @@ function Row({ currentGuess }) {
 
 
 
-
-export default function App() {
-  //test winning word
-  const winningWord = "psych";
-
-  // is there a winner state.
-const [isWinner, setIsWinner] = useState(false);
-
-// state for user input
-  const [currentGuess, setCurrentGuess] = useState([]);
-
-
-  //event handler for user input
-const handleChange = (event) => {
-  let newLetter = event.target.value.split(''); //splits input by space between letters.
-  console.log(`You Typed the Letter ${newLetter}`)
-  const newGuessArray = newLetter; //pushes split letter into its own index in array.
-  console.log(newGuessArray);
-  setCurrentGuess(newGuessArray);//sets user input as an array. Will use this array to map to Board
-};
-
-
-  return (
-<>
-<div className='title'>
-<h1>SECRET PASSWORD</h1>
-</div>
-<section className='board'>
-    <Board currentGuess={currentGuess}/>
-</section>
-<section className='user-inpu-container'>
-  <div className='user-input'>
-  <label htmlFor="currentGuess">Guess: </label>
-  <input 
-  id="currentGuess" 
-  name="currentGuess" 
-  maxLength={5}
-  type="text"  
-  onChange={handleChange} />
-</div>
-</section>
-</>
-  );
-};
+export default App;
